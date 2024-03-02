3711866d71ae596ac0aa274971ef43712909a823
弘瀬史歩 <hiroseshiho@hiroseshihonoMacBook-Air.local>
2024-03-02 21:57:23 +0900

    deviseビュー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  # トップページをホーム画面に設定
+  root to: 'homes#top'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
