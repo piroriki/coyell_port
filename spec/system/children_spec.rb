bb0ca20c79acf95335b18687ce4e5d9e75c69e77
弘瀬史歩 <hiroseshiho@hiroseshihonoMacBook-Air.local>
2024-03-09 07:38:06 +0900

    children結合テストの一覧表示機能、不具合修正

--- spec/system/children_spec.rb
+++ spec/system/children_spec.rb
@@ -2,26 +2,35 @@ require "rails_helper"
 
 describe "子供管理機能", type: :system do
   describe "一覧表示機能" do
-    before do
-      user_a = FactoryBot.create(:user, name: "ユーザーA", email: "a@example.com")
-      FactoryBot.create(:child, name: "最初の子供", gender: 0, age: 0, birthday: "2024-01-01", user: user_a)
+    # letを使用して、事前にユーザーデータを作成しておく
+    let(:user_a) { FactoryBot.create(:user, name: "ユーザーA", email: "a@example.com", password: "password") }
+    let(:user_b) { FactoryBot.create(:user, name: "ユーザーB", email: "b@example.com", password: "password") }
 
+    # beforeを使用してログイン部分共通化
+    before do
+      #user_a = FactoryBot.create(:user, name: "ユーザーA", email: "a@example.com")
+      FactoryBot.create(:child, name: "最初の子供", gender: 0, age: 0, birthday: "2024-01-01", user: user_a)
       visit login_path
-      fill_in "メールアドレス", with: "a@example.com"
-      fill_in "パスワード",     with: "password"
+      #fill_in "メールアドレス", with: "a@example.com"
+      fill_in "メールアドレス", with: login_user.email
+      #fill_in "パスワード",     with: "password"
+      fill_in "パスワード",    with: login_user.password
       click_button "ログインする"
     end
 
     context "ユーザーAがログインしている時" do
+      let!(:login_user) { user_a }
+      
       it "ユーザーAが登録した子供が表示される" do
         expect(page).to have_content "最初の子供"
       end
     end
 
     context "ユーザーBがログインしている時" do
-      before do
-        FactoryBot.create(:user, name: "ユーザーB", email: "b@example.com")
-      end
+      let!(:login_user) { user_b }
+      #before do 
+      #  FactoryBot.create(:user, name: "ユーザーB", email: "b@example.com")
+      #end
 
       it "ユーザーAが登録した子供が表示されない" do
         expect(page).to have_no_content "最初の子供"
