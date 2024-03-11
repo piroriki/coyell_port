04e820859565a9202ef45462943c04e21f1300f4
弘瀬史歩 <hiroseshiho@hiroseshihonoMacBook-Air.local>
2024-03-11 13:41:33 +0900

    height#editビュー作成

--- app/controllers/heights_controller.rb
+++ app/controllers/heights_controller.rb
@@ -39,7 +39,7 @@ class HeightsController < ApplicationController
   private
 
     def height_params
-      params.require(:height).permit(:height).merge(user_id: current_user.id)
+      params.require(:height).permit(:height).merge(child_id: params[:child_id])
     end
-    
+
 end
