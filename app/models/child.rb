697025956229d2acebbc6c6a66ac59bb5d713731
弘瀬史歩 <hiroseshiho@hiroseshihonoMacBook-Air.local>
2024-03-11 14:04:45 +0900

    childモデルとheightモデルの関連づけ追加

--- app/models/child.rb
+++ app/models/child.rb
@@ -3,6 +3,9 @@ class Child < ApplicationRecord
   # 家族ユーザーとの関連づけ
   belongs_to :user
 
+  # 各種モデルとの関連づけ
+  has_many :heights
+
   # 性別の分類をあらかじめセットしておく
   enum gender: { other: 0, male: 1, female: 2 }
 
--- app/models/height.rb
+++ app/models/height.rb
@@ -1,2 +1,6 @@
 class Height < ApplicationRecord
+
+  # childモデルとの関連づけ
+  belongs_to :child
+  
 end
