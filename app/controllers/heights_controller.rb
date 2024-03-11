b2ab38cd8a0cec2eed3735ad55cb4b208f92d7c2
弘瀬史歩 <hiroseshiho@hiroseshihonoMacBook-Air.local>
2024-03-12 06:32:31 +0900

    heights#create部分不具合修正、およびheight新規作成機能実装

--- app/controllers/heights_controller.rb
+++ app/controllers/heights_controller.rb
@@ -1,13 +1,18 @@
 class HeightsController < ApplicationController
 
+  # ネストしたモデルのため、
+  # 親モデルであるchildのインスタンスを各メソッド内に設定する
+
   def new
+    @child = Child.find(params[:child_id])
     @height = Height.new
   end
 
   def create
+    @child = Child.find(params[:child_id])
     @height = Height.new(height_params)
     if @height.save
-      redirect_to child_height_path(@child), notice: "登録しました"
+      redirect_to child_height_path(id: @height.id), notice: "登録しました"
     else
       render :new, status: :unprocessable_entity
     end
