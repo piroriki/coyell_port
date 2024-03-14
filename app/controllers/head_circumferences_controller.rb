26ce08fcfa3a5b1f4ebd59f4646abeff5c01c580
弘瀬史歩 <hiroseshiho@hiroseshihonoMacBook-Air.local>
2024-03-15 06:24:36 +0900

    head_circumferencesコントローラ各種メソッド追加

---
+++ app/controllers/head_circumferences_controller.rb
@@ -0,0 +1,49 @@
+class HeadCircumferencesController < ApplicationController
+
+  def new
+    @child              = Child.find(params[:child_id])
+    @head_circumference = HeadCircumference.new
+  end
+
+  def create
+    @child              = Child.find(params[:child_id])
+    @head_circumference = HeadCircumference.new(head_circumference_params)
+    if @head_circumference.save
+      redirect_to child_head_circumference_path(id: @head_circumference.id), notice: "登録しました"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @child              = Child.find(params[:child_id])
+    @head_circumference = HeadCircumference.find(params[:id])
+  end
+
+  def update
+    @child              = Child.find(params[:child_id])
+    @head_circumference = HeadCircumference.find(params[:id])
+    if @head_circumference.update(head_circumference_params)
+      redirect_to child_head_circumference_path(id: @head_circumference.id), notice: "更新しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @head_circumference = HeadCircumference.find(params[:id])
+    @head_circumference.destroy
+    redirect_to children_path, notice: "削除しました"
+  end
+
+  def show
+    @child              = Child.find(params[:child_id])
+    @head_circumference = HeadCircumference.find(params[:id])
+  end
+
+  private
+
+    def head_circumference_params
+      params.require(:head_circumference).permit(:time, :head_circumference, :memo).merge(child_id: params[:child_id])
+    end
+end
