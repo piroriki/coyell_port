14ef039d3d1939c891664f781c0e6c0a3c55550b
弘瀬史歩 <hiroseshiho@hiroseshihonoMacBook-Air.local>
2024-03-15 13:45:49 +0900

    mealsコントローラ各種メソッド追加

---
+++ app/controllers/meals_controller.rb
@@ -0,0 +1,49 @@
+class MealsController < ApplicationController
+
+  def new
+    @child = Child.find(params[:child_id])
+    @meal  = Meal.new
+  end
+
+  def create
+    @child = Child.find(params[:child_id])
+    @meal  = Meal.new(meal_params)
+    if @meal.save
+      redirect_to child_meal_path(id: @meal.id), notice: "登録しました"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @child = Child.find(params[:child_id])
+    @meal  = Meal.find(params[:id])
+  end
+
+  def update
+    @child = Child.find(params[:child_id])
+    @meal  = Meal.find(params[:id])
+    if @meal.update(meal_params)
+      redirect_to child_meal_path(id: @meal.id), notice: "更新しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @meal  = Meal.find(params[:id])
+    @meal.destroy
+    redirect_to children_path, notice: "削除しました"
+  end
+
+  def show
+    @child = Child.find(params[:child_id])
+    @meal  = Meal.find(params[:id])
+  end
+
+  private
+
+    def meal_params
+      params.require(:meal).permit(:kinds, :time, :memo).merge(child_id: params[:child_id])
+    end
+end
