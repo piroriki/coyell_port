7b9aa149f13fb016873670406ad7722ca4fe87d9
弘瀬史歩 <hiroseshiho@hiroseshihonoMacBook-Air.local>
2024-03-16 09:12:01 +0900

    クラススペルミス修正、およびleft_breast_milks#showリンクで指定するid不足分追加

--- app/controllers/left_breast_milks_controller.rb
+++ app/controllers/left_breast_milks_controller.rb
@@ -1,44 +1,44 @@
 class LeftBreastMilksController < ApplicationController
   
   def new
-    @child         = Child.find(params[:id])
-    @l_breast_milk = LeftBreatMilk.new
+    @child         = Child.find(params[:child_id])
+    @l_breast_milk = LeftBreastMilk.new
   end
 
   def create
-    @child         = Child.find(params[:id])
-    @l_breast_milk = LeftBreatMilk.new(l_breast_milk_params)
+    @child         = Child.find(params[:child_id])
+    @l_breast_milk = LeftBreastMilk.new(l_breast_milk_params)
     if @l_breast_milk.save
-      redirect_to child_left_breast_milk_path, notice: "登録しました"
+      redirect_to child_left_breast_milk_path(id: @l_breast_milk.id), notice: "登録しました"
     else
       render new, status: :unprocessable_entity
     end
   end
 
   def edit
-    @child         = Child.find(params[:id])
-    @l_breast_milk = LeftBreatMilk.find(params[:id])
+    @child         = Child.find(params[:child_id])
+    @l_breast_milk = LeftBreastMilk.find(params[:id])
   end
 
   def update
-    @child         = Child.find(params[:id])
-    @l_breast_milk = LeftBreatMilk.find(params[:id])
+    @child         = Child.find(params[:child_id])
+    @l_breast_milk = LeftBreastMilk.find(params[:id])
     if @l_breast_milk.update(l_breast_milk_params)
-      redirect_to child_left_breast_milk_path, notice: "更新しました"
+      redirect_to child_left_breast_milk_path(id: @l_breast_milk.id), notice: "更新しました"
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @l_breast_milk = LeftBreatMilk.find(params[:id])
+    @l_breast_milk = LeftBreastMilk.find(params[:child_id])
     @l_breast_milk.destroy
     redirect_to children_path, notice: "削除しました"
   end
 
   def show
-    @child         = Child.find(params[:id])
-    @l_breast_milk = LeftBreatMilk.find(params[:id])
+    @child         = Child.find(params[:child_id])
+    @l_breast_milk = LeftBreastMilk.find(params[:id])
   end
 
   private
