d0d1338232c9c1be5e914b6d14e750f7f7f3c81a
弘瀬史歩 <hiroseshiho@hiroseshihonoMacBook-Air.local>
2024-03-17 06:50:03 +0900

    @childのid指定を、:id → :child_idに修正

--- app/controllers/symptoms_controller.rb
+++ app/controllers/symptoms_controller.rb
@@ -1,13 +1,13 @@
 class SymptomsController < ApplicationController
 
   def new
-    @child   = Child.find(params[:id])
+    @child   = Child.find(params[:child_id])
     @symptom = Symptom.new
     @kinds_options = Symptom.kinds_i18n.invert.map{|key,value|[key,value]}
   end
 
   def create
-    @child   = Child.find(params[:id])
+    @child   = Child.find(params[:child_id])
     @symptom = Symptom.new(symptom_params)
     if @symptom.save
       redirect_to child_symptom_path, notice: "登録しました"
@@ -17,13 +17,13 @@ class SymptomsController < ApplicationController
   end
 
   def edit
-    @child   = Child.find(params[:id])
+    @child   = Child.find(params[:child_id])
     @symptom = Symptom.find(params[:id])
     @kinds_options = Symptom.kinds_i18n.invert.map{|key,value|[key,value]}
   end
 
   def update
-    @child   = Child.find(params[:id])
+    @child   = Child.find(params[:child_id])
     @symptom = Symptom.find(params[:id])
     if @symptom.update(symptom_params)
       redirect_to child_symptom_path, notice: "更新しました"
@@ -39,7 +39,7 @@ class SymptomsController < ApplicationController
   end
 
   def show
-    @child   = Child.find(params[:id])
+    @child   = Child.find(params[:child_id])
     @symptom = Symptom.find(params[:id])
   end
 
