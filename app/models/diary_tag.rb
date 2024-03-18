960f27371183529a8d37e6396cdacce7a17f9794
弘瀬史歩 <hiroseshiho@hiroseshihonoMacBook-Air.local>
2024-03-19 07:05:46 +0900

    downcase_nameメソッド追加

--- app/models/diary_tag.rb
+++ app/models/diary_tag.rb
@@ -4,6 +4,15 @@ class DiaryTag < ApplicationRecord
   has_many :diary_tag_relations, dependent: :destroy
   has_many :diaries, through: :diary_tag_relations, dependent: :destroy
 
+  # バリデーション前に、タグを全て小文字化する
+  before_validation :downcase_name
+
   # バリデーション
   validates :name, presence: true, length: { maximum:50 }
+
+  private
+
+    def downcase_name
+      self.name = name.downcase if name.present?
+    end
 end
