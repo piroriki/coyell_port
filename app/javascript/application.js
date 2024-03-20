ef1b2f0cd69baf553e4f5a579bb9bcb817c96b50
弘瀬史歩 <hiroseshiho@hiroseshihonoMacBook-Air.local>
2024-03-20 22:30:15 +0900

    非同期通信用ファイル追加

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+require("@rails/ujs").start()
+require("turbolinks"),start()
+require("@rails/activestorage").start()
+require("channels")
+reqiure("../card")
+require("jquery")
+
 import "@hotwired/turbo-rails"
 import "controllers"
