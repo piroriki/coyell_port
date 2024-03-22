2639812ebca2a53dc3cdd17373b27b34016c690f
弘瀬史歩 <hiroseshiho@hiroseshihonoMacBook-Air.local>
2024-03-22 17:54:58 +0900

    head_circumferences入力フォーム共通化

--- app/helpers/head_circumferences_helper.rb
+++ app/helpers/head_circumferences_helper.rb
@@ -1,2 +1,11 @@
 module HeadCircumferencesHelper
+ 
+  # 送信ボタン文字列切り替え用
+  def button_text
+    if    action_name == "new"
+      (t"helpers.submit.create")
+    elsif action_name == "edit"
+      (t"helpers.submit.update")
+    end
+  end
 end
