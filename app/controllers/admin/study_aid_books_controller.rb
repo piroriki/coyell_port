aecb37e734360d9cdbef85c60ef16d342a3f00dd
弘瀬史歩 <hiroseshiho@hiroseshihonoMacBook-Air.local>
2024-03-27 17:59:59 +0900

    study_aid_booksコントローラ作成

--- app/controllers/admin/study_aid_books_controller.rb
+++ app/controllers/admin/study_aid_books_controller.rb
@@ -1,65 +1,43 @@
 class Admin::StudyAidBooksController < ApplicationController
 
+  def index
+    @admin = current_user
+    @study_aid_books = StudyAidBook.all
+  end
+
   def new
-    @admin = User.find(params[:id])
-    if @admin.admin?
-      @study_aid_book.new
-    else
-      redirect_to root_path
-      flash[:danger] = "管理者権限がありません"
-    end
+    @study_aid_book = current_user.study_aid_books.new
   end
 
   def create
-    @admin = User.find(params[:id])
-    if @admin.admin?
-      @study_aid_book.new(study_aid_book_params)
-      if @study_aid_book.save
-        flash[:success] = "投稿が完了しました"
-        redirect_to admin_study_aid_books_path
-      else
-        render :new, status: :unprocessable_entity
-      end
+    @study_aid_book = current_user.study_aid_books.new(study_aid_book_params)
+    if @study_aid_book.save
+      flash[:success] = "投稿が完了しました"
+      redirect_to admin_study_aid_books_path
     else
-      redirect_to root_path
-      flash[:danger] = "管理者権限がありません"
+      render :new, status: :unprocessable_entity
     end
   end
 
   def edit
-    @admin = User.find(params[:id])
-    if @admin.admin?
-      @study_aid_book = StudyAidBook.find(params[:id])
-    else
-      redirect_to root_path
-      flash[:danger] = "管理者権限がありません"
-    end
+    @admin = current_user
+    @study_aid_book = StudyAidBook.find(params[:id])
   end
 
   def update
     @admin = User.find(params[:id])
-    if @admin.admin?
-      @study_aid_book = StudyAidBook.find(params[:id])
-      if @study_aid_book.update(study_aid_book_params)
-        flash[:success] = "編集が完了しました"
-        redirect_to admin_study_aid_books_path
-      else
-        render :edit, status: :unprocessable_entity
-      end
+    @study_aid_book = StudyAidBook.find(params[:id])
+    if @study_aid_book.save(study_aid_book_params)
+      flash[:success] = "編集が完了しました"
+      redirect_to admin_study_aid_books_path
     else
-      redirect_to root_path
-      flash[:danger] = "管理者権限がありません"
+      render :edit, status: :unprocessable_entity
     end
   end
 
   def show
-    @admin = User.find(params[:id])
-    if @admin.admin?
-      @study_aid_book = StudyAidBook.find(params[:id])
-    else
-      flash[:danger] = "管理者権限がありません"
-      redirect_to root_path
-    end
+    @admin = current_user
+    @study_aid_book = StudyAidBook.find(params[:id])
   end
 
   def destroy
