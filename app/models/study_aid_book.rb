e886115be5f96cf1255d49f4f903cfb730eee8a3
弘瀬史歩 <hiroseshiho@hiroseshihonoMacBook-Air.local>
2024-03-28 06:49:57 +0900

    ImageUploader作成、およびimageカラムと関連づけ

--- app/models/study_aid_book.rb
+++ app/models/study_aid_book.rb
@@ -3,6 +3,10 @@ class StudyAidBook < ApplicationRecord
   # ユーザーとの関連づけ
   belongs_to :user
 
+  # ImageUploaderクラスとimageカラムを紐づける
+  mount_uploader :image, ImageUploader
+  
+
   # バリデーション追加
   validates :title,   length: { maximum: 50 }
   validates :content, length: { maximum: 10000 }
